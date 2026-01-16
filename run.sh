dc468364e7bce91cc1d367c0f53ed943813467cd
Luigi Agosti <luigi@tengio.com>
2026-01-16 10:35:58 +0000

    chore: small changes

--- run.sh
+++ run.sh
@@ -2,11 +2,8 @@
 set -e
 
 # Project settings
-PROJECT_DIR="/Users/luigi/dev/prj/giago/maia-prj"
 PORT=8000
 INDEX="index.html"
-
-cd "$PROJECT_DIR"
 
 # Start simple HTTP server in background
 python3 -m http.server "$PORT"
